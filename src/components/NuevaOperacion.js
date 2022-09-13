b8f6544e3028eaadef016beb2bf771c84547927a
Braian Martinez <martinezbraian430@gmail.com>
2022-09-12 21:57:59 -0300

    Diseño de formulario

--- src/components/NuevaOperacion.js
+++ src/components/NuevaOperacion.js
@@ -8,9 +8,40 @@ const NuevaOperacion = () => {
   return (
     <div style={ estiloDiv }>
       <Sidebar />
-      <section>
-        <h1>Formulario de ingreso</h1>
-      </section>
+      <form>
+        <fieldset>
+          <section>
+            <h1>Formulario de ingreso</h1>
+          </section>
+          <section>
+              <div>
+                <p><label><span>Concepto</span></label></p>
+                <input type="text" id="input-0" name="concepto" />
+              </div>
+              <div>
+                <p><label>Fecha de ingreso </label></p>
+                <input type="date" id="input-3" name="fecha" />
+              </div>
+              <div>
+                <p><label>Tipo </label></p>
+                <select id="input-4" name="tipo">
+                  <option value="ingreso">Ingreso</option>
+                  <option value="egreso">Egreso</option>
+                </select>
+              </div>
+              <div>
+                <p><label>Monto </label></p>
+                <input type="number" id="input-2" name="monto" />
+              </div>
+          </section>
+          <section>
+            <p>
+              <button type="submit">Ingresar</button>
+              <button type="submit">Cancelar</button>
+            </p>
+          </section>
+        </fieldset>
+      </form>
     </div>
   );
 }
