bf72f9114e85ea9e81db11b0d5bc178b8932a75b
Braian Martinez <martinezbraian430@gmail.com>
2022-09-20 15:30:28 -0300

    Se agrego un array data para el manejo de pruebas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import Sidebar from './components/Sidebar';
 import './App.css';
 
 class App extends Component {
+  state = {
+    data: [
+      { id:'op-1', concept:'Empresa Bayer', date:'03/09/2022', type:'Ingreso', amount:'10000' },
+      { id:'op-2', concept:'Patente del auto', date:'20/09/2022', type:'Egreso', amount:'3000' },
+      { id:'op-3', concept:'Alquiler', date:'20/09/2022', type:'Egreso', amount:'5000' },
+    ]
+  }
   render() {
     return (
       <main className='app'>
