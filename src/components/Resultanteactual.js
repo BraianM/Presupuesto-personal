5a7a175a9d20a8785f9652d89ad0a8fdd2305575
Luciano Taddei <lucianost16@gmail.com>
2022-09-28 12:04:10 -0300

    Resultanteactual

---
+++ src/components/Resultanteactual.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../estilos/Resultante.css';
+
+function Resultanteactual () {
+    return (
+       
+        <div className='contenedor-actual'>
+        <div className='contenedor-texto'>
+            <p className='dinero-resultante'>
+                Dinero Resultante $990
+            </p>
+        </div>
+        </div>
+    );
+}
+export default Resultanteactual;
