752d97e424697a879eef63d4a46d650d78e24ac1
Braian Martinez <martinezbraian430@gmail.com>
2022-10-14 20:19:57 -0300

    Acomodando la identacion del codigo

--- src/components/Resultanteactual.js
+++ src/components/Resultanteactual.js
@@ -2,15 +2,14 @@ import React from 'react';
 import '../estilos/Resultante.css';
 
 function Resultanteactual () {
-    return (
-       
-        <div className='contenedor-actual'>
-        <div className='contenedor-texto'>
-            <p className='dinero-resultante'>
-                Dinero Resultante $990
-            </p>
-        </div>
-        </div>
-    );
+  return (
+    <div className='contenedor-actual'>
+      <div className='contenedor-texto'>
+        <p className='dinero-resultante'>
+          Dinero Resultante $990
+        </p>
+      </div>
+    </div>
+  );
 }
 export default Resultanteactual;
